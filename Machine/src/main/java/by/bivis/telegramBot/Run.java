77ac5bc2784e65ee1d77d0a9194b1e95c3e9dc87
Bivis-s <typoborov@yandex.by>
2020-11-20 15:57:45 +0300

    now it has normal interface and may add subscriptions

--- Machine/src/main/java/by/bivis/telegramBot/Run.java
+++ Machine/src/main/java/by/bivis/telegramBot/Run.java
@@ -5,10 +5,11 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class Run {
 
-    public static void main(String[] args) throws IOException, TelegramApiException {
+    public static void main(String[] args) throws IOException, TelegramApiException, SQLException {
         TgBot tgBot = new TgBot();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
 
